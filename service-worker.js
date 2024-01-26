125bd4c0f9a34e3d6c57d693498015735dd53673
barpaw <barpaw@gmail.com>
2024-01-26 10:32:06 +0100

    feat: pwa stuff

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 self.addEventListener('install', function(e) {
+  console.log("Service Worker installing.");
     e.waitUntil(
       caches.open('sejm-stream-cache').then(function(cache) {
         return cache.addAll([
@@ -9,7 +10,11 @@ self.addEventListener('install', function(e) {
       })
     );
   });
-  
+    
+  self.addEventListener("activate", event => {
+    console.log("Service Worker activating.");
+  });
+
   self.addEventListener('fetch', function(event) {
     event.respondWith(
       caches.match(event.request).then(function(response) {
